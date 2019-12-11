b0a9c997ed6e7f2dcd971ad2352cebbeba5f2676
karolcosta <karolinycosta@yahoo.com.br>
2019-12-10 21:43:22 -0300

    inicio do projeto

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //inicia o srvidor
 const app = require("./src/app");
-const port = 3001;
+const port = process.env.port || 3001;
 
 app.listen(port, function(){
     console.log(`app esta rodando na porta ${port}`)
